d5ad29d47dc4520f316b0720e6575b015cf33f4a
sijibomi <gbemilesanmi@gmail.com>
2023-06-19 23:39:03 +0100

    notes

---
+++ theClang/converge.c
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+
+void conv (int line , char *message);
+
+int main(){
+
+}
+
+void conv (int line , char *message){
+    int i , j;
+    for(i=0 , j=strlen(message) ; i<j ; i++ , j--){
+    }
+}
