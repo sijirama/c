f7712f8e18602875db38e9fd8db76ba8c60cb6e5
sijibomi <gbemilesanmi@gmail.com>
2023-08-24 01:06:47 +0100

    oka

---
+++ IOfunctions/main.c
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main() {
+
+    // char cgets(char *str)
+    // cgets, used to read string from stdin/the keyboard
+    // char arr[30];
+    // arr[0] = 20; // must set the first byte to the max length of strings to
+    // save cputs("Enter a string: "); cgets(arr); // Documents and Settings
+
+    //----------------------------------------------------------------
+    // void clearerr(FILE * stream)
+    //
+}
