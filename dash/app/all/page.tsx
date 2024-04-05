01707034063371024843faea381186c24a295f99
sijirama <gbemilesanmi@gmail.com>
2024-04-06 00:05:13 +0100

    lfg

--- dash/app/all/page.tsx
+++ dash/app/all/page.tsx
@@ -19,15 +19,15 @@ export default function Page() {
         <main className=" min-h-screen bg-zinc-50 flex flex-col">
             <section className='bg-slate-100 '>
                 <Header showInput={true} className='bg-slate-100' />
-                <div className=' w-full md:w-10/12 mx-auto flex items-center h-full flex-col gap-3'>
-                    <div className='flex items-center justify-between w-full'>
-                        <div>
-                            <p className='text-2xl leading-10 -tracking-wide font-semibold'>Bonny and Clyde Street, Ajao Estate, Lagos. </p>
-                            <p>Reviews (People are raving about the selected location)</p>
+                <div className=' w-10/12 mx-auto flex items-center h-full flex-col gap-3'>
+                    <div className='flex flex-col md:flex-row gap-4 items-center justify-between w-full'>
+                        <div className='text-center md:text-left'>
+                            <p className='text-xl md:text-2xl leading-10 -tracking-wide font-semibold'>Bonny and Clyde Street, Ajao Estate, Lagos. </p>
+                            <p className='text-sm md:text-base'>Reviews (People are raving about the selected location)</p>
                         </div>
-                        <div className='flex items-center gap-2'>
-                            <Button onClick={onClick} className='px-7 bg-[#3366FF] rounded-sm'>LEAVE A REVIEW</Button>
-                            <Button onClick={onClick} className='px-4 bg-transparent border-2 border-[#3366FF] rounded-lg'>
+                        <div className='flex items-center gap-2 '>
+                            <Button onClick={onClick} className='px-1 text-xs md:text-base md:px-7 bg-[#3366FF] rounded-sm'>LEAVE A REVIEW</Button>
+                            <Button onClick={onClick} className=' px-4 bg-transparent border-2 border-[#3366FF] rounded-lg'>
                                 <CiBookmark className='text-2xl font-semibold text-[#3366FF]' />
                             </Button>
                             <Button onClick={onClick} className='px-4 bg-transparent border-2 border-[#3366FF] rounded-lg'>
